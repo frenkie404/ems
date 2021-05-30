f0be202f8f0cee957ff302fd88bc0db59cb50ca7
frenkie404 <dejongf53@gmail.com>
2021-05-30 11:29:49 +0545

    fix: invalid credentials now redirect to errors page for admin

--- controllers/authenticate/validate_admin.php
+++ controllers/authenticate/validate_admin.php
@@ -6,5 +6,8 @@ function validate_admin($form_data)
     if ($username === "admin" && $password === "admin") {
         set_session("logged_in_as", "admin");
         redirect("/dashboard.php");
+    } else {
+        set_session("error_code", 401);
+        redirect("/error.php");
     }
 }

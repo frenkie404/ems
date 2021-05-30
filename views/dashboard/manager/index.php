f6dcee52c847deda304a64a7da2f182facb58dcd
frenkie404 <dejongf53@gmail.com>
2021-05-30 17:25:51 +0545

    fix: resolves error when employee count was 0 on manager dashboard

--- views/dashboard/manager/index.php
+++ views/dashboard/manager/index.php
@@ -10,8 +10,11 @@ if ($is_verified === '0') {
 } else {
     $employees = get_session("employees");
 
-    if (!$employees) {
+    if ($employees === null) {
         redirect("/controllers/manager/getEmployees.php");
+        return;
+    } else if (count($employees) === 0) {
+        echo "<h3>No employees found<h3>";
         return;
     }
     output_table(["#", "First Name", "Last Name", "Email", "Address", "Salary", "Bonus", "Date of Joining", "Actions"], $employees, "manager");
